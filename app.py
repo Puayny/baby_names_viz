4a6bc2720efb7e74b4e866fd4a4924a7adedf0d0
Puay Ni Yi <puayny@gmail.com>
2020-11-25 20:15:12 +0800

    Revert troubleshooting changes in app.pyg"
    This reverts commit cd20df329632e8ae152ec986cc803fb9f41c4b3b.

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def load_data(data_to_load, prereq_data=None):
 
         baby_names.set_index(["year", "gender", "name"], inplace=True)
         baby_names.sort_values(
-            by=["year", "gender", "count"], ascending=[True, True, False], inplace=True
+            by=["year", "gender", "count"], ascending=[True, True, False]
         )
         return baby_names
     elif data_to_load == "biblical_names":
